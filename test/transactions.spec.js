c68928575c3764e28b7becf6e6b2ea7c9a1326d5
Justin Soliz <justinsoliz@gmail.com>
2017-04-10 17:57:16 -0700

    fixing specs against new client

--- test/transactions.spec.js
+++ test/transactions.spec.js
@@ -3,9 +3,10 @@
  * file: test/transactions.spec.js
  */
 
-import Avatax from '../';
+// import Avatax from '../';
+import Avatax from '../lib/AvaTaxClient';
 import nock from 'nock';
-import { account, licenseKey, companyCode } from './test_creds';
+// import { account, licenseKey, companyCode } from './test_creds';
 import transactionRequest from './fixtures/transaction_request';
 import transactionResponse from './fixtures/transaction_response';
 import transactionsListResponse from './fixtures/transactions_list_response';
@@ -13,13 +14,36 @@ import transactionsListResponse from './fixtures/transactions_list_response';
 const baseUrl = 'https://sandbox-rest.avatax.com';
 
 describe('Avatax Transactions', () => {
-  const client = new Avatax({ account, licenseKey });
+  const username = '';
+  const password = '';
+  const appName = 'myapp';
+  const appVersion = '1.0';
+  const environment = 'sandbox';
+  const machineName = 'mbp';
+
+  const client = new Avatax({ appName, appVersion, environment, machineName })
+    .withSecurity({ username, password });
+  // const client = new Avatax({ account, licenseKey });
 
   describe('Listing transactions by company', () => {
     beforeEach(() => {
       nock(baseUrl).get(`/api/v2/companies/${companyCode}/transactions`)
         .reply(200, transactionsListResponse);
     })
+    
+    it.only('should resolve address', () => {
+      const address = {
+        city: 'irvine',
+        postalCode: '92615',
+        region: 'ca',
+        country: 'us'
+      };
+
+      return client.resolveAddress(address)
+        .then(res => {
+          console.log(res);
+        });
+    });
 
     it('should list transactions by company code', () => {
       return client.listTransactions({ companyCode })
