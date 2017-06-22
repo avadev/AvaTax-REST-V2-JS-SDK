93d6bdbdc0f088c6c3c676c2fcbd17b797f23e63
Justin Soliz <justinsoliz@gmail.com>
2017-06-22 08:33:18 -0700

    setup formatter

--- test/fixtures/adjust_transaction_request.js
+++ test/fixtures/adjust_transaction_request.js
@@ -3,35 +3,35 @@
  * file: test/fixtures/adjust_transaction_request.js
  */
 export default {
-    "adjustmentReason": "PriceAdjusted",
-    "adjustmentDescription": "Price dropped before shipping",
-    "newTransaction": {
-        "type": "SalesInvoice",
-        "companyCode": "abc123",
-        "date": "2017-04-13",
-        "customerCode": "ABC",
-        "purchaseOrderNo": "2017-04-13-001",
-        "addresses": {
-            "SingleLocation": {
-                "line1": "123 Main Street",
-                "city": "Irvine",
-                "region": "CA",
-                "country": "US",
-                "postalCode": "92615"
-            }
-        },
-        "lines": [
-            {
-                "number": "1",
-                "quantity": 1,
-                "amount": 90,
-                "taxCode": "PS081282",
-                "itemCode": "Y0001",
-                "description": "Yarn"
-            }
-        ],
-        "commit": true,
-        "currencyCode": "USD",
-        "description": "Yarn"
-    }
-}
+  adjustmentReason: 'PriceAdjusted',
+  adjustmentDescription: 'Price dropped before shipping',
+  newTransaction: {
+    type: 'SalesInvoice',
+    companyCode: 'abc123',
+    date: '2017-04-13',
+    customerCode: 'ABC',
+    purchaseOrderNo: '2017-04-13-001',
+    addresses: {
+      SingleLocation: {
+        line1: '123 Main Street',
+        city: 'Irvine',
+        region: 'CA',
+        country: 'US',
+        postalCode: '92615'
+      }
+    },
+    lines: [
+      {
+        number: '1',
+        quantity: 1,
+        amount: 90,
+        taxCode: 'PS081282',
+        itemCode: 'Y0001',
+        description: 'Yarn'
+      }
+    ],
+    commit: true,
+    currencyCode: 'USD',
+    description: 'Yarn'
+  }
+};
