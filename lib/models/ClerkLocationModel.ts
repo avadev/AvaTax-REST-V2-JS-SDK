c4e9d8ce897314c1595382fb9db16e1b95d71af4
svc-developer <svc-developer@avalara.com>
2025-06-26 16:19:40 +0000

    25.6.2

---
+++ lib/models/ClerkLocationModel.ts
@@ -0,0 +1,76 @@
+/*
+ * AvaTax Software Development Kit for JavaScript
+ *
+ * (c) 2004-2022 Avalara, Inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author     Jonathan Wenger <jonathan.wenger@avalara.com>
+ * @author     Sachin Baijal <sachin.baijal@avalara.com>
+ * @copyright  2004-2018 Avalara, Inc.
+ * @license    https://www.apache.org/licenses/LICENSE-2.0
+ * @link       https://github.com/avadev/AvaTax-REST-V2-JS-SDK
+ */
+
+import * as Enums from '../enums/index';
+import { StateModel } from "./StateModel";
+import { JsonObject, JsonProperty } from "json2typescript";
+import { DateConverter } from "../utils/dateConverter";
+
+/**
+ * Represents a physical location used in exemption certificate management.
+ * @export
+ * @class ClerkLocationModel
+ */
+ @JsonObject("ClerkLocationModel")
+ export class ClerkLocationModel {
+    /**
+     * @type {number}
+     * @memberof ClerkLocationModel
+     */
+   @JsonProperty("id", Number, true)
+   id?: number | undefined = undefined;
+    /**
+     * @type {string}
+     * @memberof ClerkLocationModel
+     */
+   @JsonProperty("name", String, true)
+   name?: string | undefined = undefined;
+    /**
+     * @type {string}
+     * @memberof ClerkLocationModel
+     */
+   @JsonProperty("code", String, true)
+   code?: string | undefined = undefined;
+    /**
+     * @type {string}
+     * @memberof ClerkLocationModel
+     */
+   @JsonProperty("address_line1", String, true)
+   address_line1?: string | undefined = undefined;
+    /**
+     * @type {string}
+     * @memberof ClerkLocationModel
+     */
+   @JsonProperty("address_line2", String, true)
+   address_line2?: string | undefined = undefined;
+    /**
+     * @type {string}
+     * @memberof ClerkLocationModel
+     */
+   @JsonProperty("city", String, true)
+   city?: string | undefined = undefined;
+    /**
+     * @type {string}
+     * @memberof ClerkLocationModel
+     */
+   @JsonProperty("zip", String, true)
+   zip?: string | undefined = undefined;
+    /**
+     * @type {StateModel}
+     * @memberof ClerkLocationModel
+     */
+   @JsonProperty("state", StateModel, true)
+   state?: StateModel | undefined = undefined;
+ }
---
+++ lib/models/ItemImageDetailOutputModel.ts
@@ -0,0 +1,81 @@
+/*
+ * AvaTax Software Development Kit for JavaScript
+ *
+ * (c) 2004-2022 Avalara, Inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author     Jonathan Wenger <jonathan.wenger@avalara.com>
+ * @author     Sachin Baijal <sachin.baijal@avalara.com>
+ * @copyright  2004-2018 Avalara, Inc.
+ * @license    https://www.apache.org/licenses/LICENSE-2.0
+ * @link       https://github.com/avadev/AvaTax-REST-V2-JS-SDK
+ */
+
+import * as Enums from '../enums/index';
+import { JsonObject, JsonProperty } from "json2typescript";
+import { DateConverter } from "../utils/dateConverter";
+
+/**
+ * Item image output model with the image URL
+ * @export
+ * @class ItemImageDetailOutputModel
+ */
+ @JsonObject("ItemImageDetailOutputModel")
+ export class ItemImageDetailOutputModel {
+    /**
+     * @type {string}
+     * @memberof ItemImageDetailOutputModel
+     */
+   @JsonProperty("imageUrl", String, true)
+   imageUrl?: string | undefined = undefined;
+    /**
+     * @type {string}
+     * @memberof ItemImageDetailOutputModel
+     */
+   @JsonProperty("itemImageId", String, true)
+   itemImageId?: string | undefined = undefined;
+    /**
+     * @type {number}
+     * @memberof ItemImageDetailOutputModel
+     */
+   @JsonProperty("itemImageDetailId", Number, true)
+   itemImageDetailId?: number | undefined = undefined;
+    /**
+     * @type {number}
+     * @memberof ItemImageDetailOutputModel
+     */
+   @JsonProperty("companyId", Number, true)
+   companyId?: number | undefined = undefined;
+    /**
+     * @type {number}
+     * @memberof ItemImageDetailOutputModel
+     */
+   @JsonProperty("itemId", Number, true)
+   itemId?: number | undefined = undefined;
+    /**
+     * @type {Date}
+     * @memberof ItemImageDetailOutputModel
+     */
+   @JsonProperty("createdAt", DateConverter, true)
+   createdAt?: Date | undefined = undefined;
+    /**
+     * @type {number}
+     * @memberof ItemImageDetailOutputModel
+     */
+   @JsonProperty("createdUserId", Number, true)
+   createdUserId?: number | undefined = undefined;
+    /**
+     * @type {Date}
+     * @memberof ItemImageDetailOutputModel
+     */
+   @JsonProperty("modifiedAt", DateConverter, true)
+   modifiedAt?: Date | undefined = undefined;
+    /**
+     * @type {number}
+     * @memberof ItemImageDetailOutputModel
+     */
+   @JsonProperty("modifiedUserId", Number, true)
+   modifiedUserId?: number | undefined = undefined;
+ }
